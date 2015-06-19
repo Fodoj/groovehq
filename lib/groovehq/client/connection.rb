0c163bdd37a4e295d701c75585873f88706a7776
Kirill Shirinkin <fodojyko@gmail.com>
2015-06-19 11:07:05 +0200

    Add pagination and tickets_count support

--- lib/groovehq/client/connection.rb
+++ lib/groovehq/client/connection.rb
@@ -20,18 +20,39 @@ module GrooveHQ
         request :delete, path, options
       end
 
+      private
+
       def request(method, path, options)
         response = self.class.send(method, path, { query: options })
         data = response.parsed_response
         parse_data(data)
       end
 
-      def parse_data(data)
-        return unless data
-        data = data.values.first
+      def parse_data(original_data)
+        return unless original_data
+
+        # This hack is added primaraly to deal with API response inconsistencies:
+        # https://www.groovehq.com/docs/ticket-counts#listing-ticket-counts (one key per folder)
+        # https://www.groovehq.com/docs/tickets#listing-tickets (has two keys: 'tickets' and 'meta')
+        # https://www.groovehq.com/docs/tickets#finding-one-ticket (one key: 'ticket')
+        # :(
+        data = original_data.keys.count <= 2 ? original_data.values.first : original_data
+
         case data
         when Hash  then Resource.new(self, data)
-        when Array then data.map { |hash| parse_data({resource: hash}) }
+        when Array then Resource.new(self,
+          {
+            collection: data.map { |hash| parse_data({resource: hash}) },
+            meta: original_data["meta"],
+            links: {
+              next: {
+                href: original_data["meta"]["pagination"]["next_page"]
+              },
+              prev: {
+                href: original_data["meta"]["pagination"]["prev_page"]
+              }
+            }
+          })
         when nil   then nil
         else data
         end
