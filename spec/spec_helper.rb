50e6b3c3391bd3e67ec47a4d28536ec8fd60e7aa
Kirill Shirinkin <fodojyko@gmail.com>
2016-01-31 17:45:07 +0100

    Fix nonsense spec config

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,10 +4,6 @@ require_relative '../lib/groovehq'
 RSpec.configure do |config|
 
   config.before :all, integration: true do
-    WebMock.enable!
-  end
-
-  config.before :all, integration: true do
     WebMock.disable!
   end
 
