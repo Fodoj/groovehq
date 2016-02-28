4999acdd73e5e3d161a3c1486549798b7ec7a368
Ross Kaffenberger <rosskaff@gmail.com>
2016-02-28 14:31:13 -0500

    Call WebMock.enable before all specs in an example group

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,9 +2,11 @@ require 'webmock/rspec'
 require_relative '../lib/groovehq'
 
 RSpec.configure do |config|
+  config.before :all do
+    WebMock.enable!
+  end
 
   config.before :all, integration: true do
     WebMock.disable!
   end
-
 end
